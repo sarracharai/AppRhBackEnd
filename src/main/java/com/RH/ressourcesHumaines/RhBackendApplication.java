1d17f48c4b667912d217ea8f500424474f542320
sarra <sarracharai123@gmail.com>
2024-03-01 19:07:56 +0100

    First commit backend

---
+++ src/main/java/com/RH/ressourcesHumaines/RhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.RH.ressourcesHumaines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhBackendApplication.class, args);
+	}
+
+}
